fd97a1011c3a990d38693fd28a471d995fa94a1a
parkdn4 <pdaeun0903@gmail.com>
2026-09-17 03:15:39 +0900

    Q3: Airflow compose 환경 구축 및 sample_dag 작성

---
+++ dags/sample_dag.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+def hello():
+    return "Hello Airflow"
+
+
+def goodbye():
+    return "Goodbye Airflow"
+
+
+with DAG(
+    dag_id="sample_dag",
+    start_date=datetime(2026, 9, 16),
+    schedule="@daily",
+    catchup=False,
+) as dag:
+
+    start = EmptyOperator(
+        task_id="start"
+    )
+
+    hello_task = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello
+    )
+
+    goodbye_task = PythonOperator(
+        task_id="goodbye_task",
+        python_callable=goodbye
+    )
+
+    end = EmptyOperator(
+        task_id="end"
+    )
+
+    start >> hello_task >> goodbye_task >> end
